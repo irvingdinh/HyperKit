8ca015a0a2cafdb9dbb76d20f7a20c62dbadfca7
Irving Dinh <irving.dinh@gmail.com>
2023-09-29 11:11:06 +0700

    Initial commit

---
+++ Assets/Features/Home/Scenes/HomeSceneManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Lorem ipsum dolor sit amet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
